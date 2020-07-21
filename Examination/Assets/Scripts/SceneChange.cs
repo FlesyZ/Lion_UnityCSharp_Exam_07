9af967ad7021bf1d68186b415374b0d0271486f1
FlesyZ <63327364+FlesyZ@users.noreply.github.com>
2020-07-21 15:27:43 +0800

    期末測驗 v2

--- Examination/Assets/Scripts/SceneChange.cs
+++ Examination/Assets/Scripts/SceneChange.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Global
 {
     public class SceneChange : MonoBehaviour
     {
+        public Text[] tooltips = new Text[2];
+
         int scene = 1;
 
         void Awake()
@@ -14,7 +17,7 @@ namespace Global
 
         void Start()
         {
-            SceneManager.LoadScene(1);
+            SceneChanger(scene);
         }
 
         void Update()
@@ -22,13 +25,37 @@ namespace Global
             if (Input.GetKeyDown(KeyCode.O))
             {
                 if (scene > 1)
-                    SceneManager.LoadScene("第" + --scene + "題");
+                    SceneChanger(--scene);
+                else
+                    SceneChanger(scene = 5);
             }
             else if (Input.GetKeyDown(KeyCode.P))
             {
                 if (scene < 5)
-                    SceneManager.LoadScene("第" + ++scene + "題");
+                    SceneChanger(++scene);
+                else
+                    SceneChanger(scene = 1);
             }
+        }
+
+        void SceneChanger(int scene)
+        {
+            switch (scene)
+            {
+                case 1:
+                    tooltips[0].text = "[Ｏ] 跳到最後一題";
+                    tooltips[1].text = "\n[Ｐ] 跳到下一題";
+                    break;
+                case 5:
+                    tooltips[0].text = "[Ｏ] 跳回上一題";
+                    tooltips[1].text = "\n[Ｐ] 跳回第一題";
+                    break;
+                default:
+                    tooltips[0].text = "[Ｏ] 跳回上一題";
+                    tooltips[1].text = "\n[Ｐ] 跳到下一題";
+                    break;
+            }
+            SceneManager.LoadScene("第" + scene + "題");
         }
     }
 }
